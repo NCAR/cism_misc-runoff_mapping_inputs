0c398f0ab20257b42c1b26608ae48f5539614fd3
Bill Sacks <sacks@ucar.edu>
2017-10-30 10:28:52 -0600

    add files to make gl4 to gx1v6, and make_mapping_files for gx3v7

---
+++ make_mapping_files_gl4_gx1v6.sh
@@ -0,0 +1,6 @@
+./runoff_map < runoff_map_gl4_gx1v6.nml
+./run_merge_mapping_files.sh \
+--map_in_oo map_gland4km_epsg3413_to_gx1v6_nn.nc \
+--map_in_ms map_gland4km_to_gx1v6_nnsm_e1000r300_171027.nc \
+--region_mask /glade/p/cesmdata/cseg/inputdata/ocn/pop/gx1v6/grid/region_mask_20090205.ieeei4 \
+--map_out map_gland4km_to_gx1v6_nn_open_ocean_nnsm_e1000r300_marginal_sea_171027.nc
