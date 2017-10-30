451ea1a4a34a17adf53a58ff2e5a12beb4d4d368
Bill Sacks <sacks@ucar.edu>
2017-10-30 10:45:20 -0600

    Add files for gland5

---
+++ make_mapping_files_gl5_gx3v7.sh
@@ -0,0 +1,6 @@
+./runoff_map < runoff_map_gl5_gx3v7.nml
+./run_merge_mapping_files.sh \
+--map_in_oo map_gland5km_to_gx3v7_nn.nc \
+--map_in_ms map_gland5km_to_gx3v7_nnsm_e1000r500_171030.nc \
+--region_mask /glade/p/cesmdata/cseg/inputdata/ocn/pop/gx3v7/grid/region_mask_20090831.ieeei4 \
+--map_out map_gland5km_to_gx3v7_nn_open_ocean_nnsm_e1000r500_marginal_sea_171030.nc
