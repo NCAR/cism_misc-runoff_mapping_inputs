5cd49c847f880c152a3c24ca38dca33d50327400
Bill Sacks <sacks@ucar.edu>
2017-11-05 11:57:09 -0700

    Update date stamps

--- make_mapping_files_gl4_gx3v7.sh
+++ make_mapping_files_gl4_gx3v7.sh
@@ -1,6 +1,6 @@
 ./runoff_map < runoff_map_gl4_gx3v7.nml
 ./run_merge_mapping_files.sh \
 --map_in_oo map_gland4km_epsg3413_to_gx3v7_nn.nc \
---map_in_ms map_gland4km_to_gx3v7_nnsm_e1000r500_171030.nc \
+--map_in_ms map_gland4km_to_gx3v7_nnsm_e1000r500_171105.nc \
 --region_mask /glade/p/cesmdata/cseg/inputdata/ocn/pop/gx3v7/grid/region_mask_20090831.ieeei4 \
---map_out map_gland4km_to_gx3v7_nn_open_ocean_nnsm_e1000r500_marginal_sea_171030.nc
+--map_out map_gland4km_to_gx3v7_nn_open_ocean_nnsm_e1000r500_marginal_sea_171105.nc
